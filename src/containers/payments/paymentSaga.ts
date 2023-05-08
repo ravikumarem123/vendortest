9b5a1927dc0d7982539b10575f658f7b4b5bd1ca
Saurav Bhagat <saurav.bhagat@jumbotail.com>
2023-05-08 11:06:00 +0530

    VP-4: changed info message

--- src/containers/payments/paymentSaga.ts
+++ src/containers/payments/paymentSaga.ts
@@ -153,7 +153,7 @@ function* createAndDownloadFile(
     try {
         const worksheet = utils.json_to_sheet(data);
         const workbook = utils.book_new();
-        utils.book_append_sheet(workbook, worksheet, 'Dates');
+        utils.book_append_sheet(workbook, worksheet, 'Settlement Details');
         writeFileXLSX(workbook, `${fileName}.xlsx`, { compression: true });
     } catch (err) {
         console.log('error in creating sheet: ', err);
